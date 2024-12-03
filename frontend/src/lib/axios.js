7759b338343b276fa9964c6033f435745e77e340
sujalmendhe02 <132379642+sujalmendhe02@users.noreply.github.com>
2024-12-03 23:14:05 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://skinsify-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
